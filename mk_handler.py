8fbaf191805eb7cabf1a2756be6bb59a33a25434
a.seo <akimitsu.seo@dragon.jp>
2017-12-23 21:43:18 +0900

    Update mk_handler.py

--- mk_handler.py
+++ mk_handler.py
@@ -13,7 +13,7 @@ URL = getenv('URL')
 def respond(err, res=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
+        'body': err if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
         },
